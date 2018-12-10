6c409f4c616de990372a160868889569de72c59c
FosterSamuel <samuelfosterweb@gmail.com>
2018-12-10 12:10:49 -0800

    Updated MapReduce java code and JAR

--- MapReduceDuplicateCount.java
+++ MapReduceDuplicateCount.java
@@ -22,15 +22,15 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.log4j.Logger;
 
 public class MapReduceDuplicateCount {
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "Duplicate Count");
-		
+
 		job.setJarByClass(MapReduceDuplicateCount.class);
 		job.setMapperClass(DuplicateCountMapper.class);
+		job.setCombinerClass(DuplicateCountReducer.class);
 		job.setReducerClass(DuplicateCountReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
@@ -50,42 +50,36 @@ public class MapReduceDuplicateCount {
 	 */
 	public static class DuplicateCountMapper extends Mapper<Object, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
-		private Logger log = Logger.getLogger(DuplicateCountMapper.class);
-		
 		private Text word = new Text();
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			log.info("Value: " + value);
 			String text = value.toString();
 			if (text.contains("PostHistoryTypeId=\"10\"") && text.contains("OriginalQuestionIds")) {
-				text = text.split("[\\[\\]]")[1];
-				
-				log.info("Text after first split: " + text);
-				
-				for(String linkId : text.split(",")) {
-					word.set("ID" + linkId.replaceAll("\\s", ""));
-					log.info("Word: " + word);
+				text = text.split("[\\[\\]]")[1]; // Find the list of question Ids
+
+				for (String linkId : text.split(",")) {
+					word.set(linkId.replaceAll("\\s", "")); // Remove whitespace
 					context.write(word, one);
 				}
 			}
-			log.info("=================");
 		}
 	}
 
 	/**
-	 * With the original Ids in place, we reduce them to count
-	 * them. Output to file.
+	 * With the original Ids in place, we reduce them to count them. Output to
+	 * file.
 	 */
 	public static class DuplicateCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 		private IntWritable result = new IntWritable();
 
 		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			int sum = 0;
+			int count = 0;
+
 			for (IntWritable val : values) {
-				sum += val.get();
+				count += val.get();
 			}
-			
-			result.set(sum);
+
+			result.set(count);
 			context.write(key, result);
 		}
 	}
